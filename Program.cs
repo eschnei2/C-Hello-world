fd3d2ab238f2d0a9c34c0ac8056c15e16790f19b
eschnei2 <everettschneider@gmail.com>
2021-08-20 11:15:02 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Who would you like to say hello to? ");
+
+            string name = System.Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                System.Console.WriteLine("Fine, don't say 'hello'!");
+            }
+            else
+            {
+                System.Console.WriteLine($"Hello, {name}!");
+            }
+        }
+    }
+}
